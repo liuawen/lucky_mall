96d334f56081de46ffe4af0fa66a698f3ccc75ab
liuawen <157514367@qq.com>
2020-05-11 13:18:06 +0800

    Initial commit

---
+++ src/main/java/com/awen/lucky_mall/LuckyMallApplication.java
@@ -0,0 +1,13 @@
+package com.awen.lucky_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awen/lucky_mall/LuckyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awen.lucky_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
